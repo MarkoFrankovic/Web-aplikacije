e0755b19ddd60c74f51964c8ca1fa987a141d331
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 20:24:59 +0100

    Zadaća 1

---
+++ src/views/Signup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="signup">
+    <h1>Registracija</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Upiši mail" />
+              <small id="emailHelp" class="form-text text-muted" >Nemoj djelit s drugima svoje podatke.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Ime</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Upiši željeno ime" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Confirm Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password" />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+
+          <div class="jumbotron">
+  <h1 class="display-4">Dobrodošao!</h1>
+  <p class="lead">Pozdrav novi korisniče!</p>
+  <hr class="my-4">
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Više</a>
+  </p>
+</div>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
